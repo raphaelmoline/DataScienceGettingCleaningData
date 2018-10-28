c4cf5be48401aff210863ff597e95b57ed55d0a1
raphaelmoline <raphael.moline@gmail.com>
2018-10-28 19:29:49 +0800

    some perf test - to improve

--- DataDownloads.R
+++ DataDownloads.R
@@ -11,3 +11,15 @@ library("readxl")
 gas <- read_excel("NaturalGas.xlxs")
 dat <- gas[18:22,7:15] # no header so adjusting the row
 # excel library to be used is not working in current Rstudio version, skipping for now
+
+
+xmlUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+library("httr")
+library("XML")
+restaurant <- xmlTreeParse(GET(xmlUrl),useInternal=TRUE)
+rootNode <- xmlRoot(restaurant)
+zipcodes <- xpathSApply(rootNode,"//zipcode",xmlValue)
+length(zipcodes[zipcodes==21231])
+
+dtUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(dtUrl , destfile = "survey.csv", method="curl")
