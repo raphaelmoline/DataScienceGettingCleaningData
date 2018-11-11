ee6084f8f61a0b896bc409d2a94fbf80e2ab9fc3
raphaelmoline <raphael.moline@gmail.com>
2018-11-11 15:08:58 +0800

    add mysql example

--- MySQL.R
+++ MySQL.R
@@ -1,2 +1,16 @@
-ucscDb <- dbConnect(MySQL(), user="genome", host="genome-mysql.cse.ucsc,edu")
-result <- dbGetQuery(ucsDb,"show databases,") ; dbDisconnect(ucscDB);
+library("DBI")
+library("RMySQL")
+ucscDb <- dbConnect(MySQL(), user="genome", host="genome-mysql.cse.ucsc.edu")
+result <- dbGetQuery(ucscDb,"show databases;")
+dbDisconnect(ucscDb)
+result
+
+hg19 <- dbConnect(MySQL(), user="genome", db="hg19",host="genome-mysql.cse.ucsc.edu")
+allTables <- dbListTables(hg19)
+length(allTables)
+dbListFields(hg19,"affyU133Plus2")
+dbGetQuery(hg19, "select count(*) from affyU133Plus2")
+
+affyData <- dbReadTable(hg19,"affyU133Plus2")
+head(affyData)
+dbDisconnect(hg19)
