da0c52befc987654ca17be358d5f5e910e037e7e
raphaelmoline <raphael.moline@gmail.com>
2018-11-18 16:28:33 +0800

    summarizing data

--- week3.R
+++ week3.R
@@ -8,3 +8,51 @@ x[(x$var1 <=3 & x$var3 >11),] # subsetting on conditions
 # manage NAs with which
 x[x$var2 > 8,]
 x[which(x$var2 > 8),]
+
+#Sort
+sort(x$var1)
+sort(x$var1,decreasing = TRUE)
+sort(x$var2, na.last = TRUE)
+
+#Order
+x[order(x$var1),] #order returns rows references
+x[order(x$var1,x$var3),] # multiple
+
+# using plyr library
+library(plyr)
+arrange(x,var1)
+arrange(x,desc(var1))
+
+# add columns to data frame
+x$var4 <- rnorm(5)
+y <- cbind(x,rnorm(5))
+
+if(!file.exists("./data")) {dir.create("./data")}
+fileUrl <- "https://data.baltimorecity.gov/api/views/k5ry-ef3g/rows.csv?accessType=DOWNLOAD"
+download.file(fileUrl,destfile="./data/restaurants.csv")
+restData <- read.csv("./data/restaurants.csv")
+
+# sample of the data
+head(restData,5)
+tail(restData,5)
+
+# summary of the data
+summary(restData)
+str(restData)
+quantile(restData$councilDistrict, na.rm=TRUE)
+quantile(restData$councilDistrict, probs = c(0.5,0.75,0.9))
+table(restData$zipCode,useNA = "ifany") # would create a columns with the # of NAs
+table(restData$councilDistrict,restData$zipCode) # two dimensional table
+
+# check for missing values
+sum(is.na(restData$councilDistrict))
+any(is.na(restData$councilDistrict))
+all(restData$zipCode > 0)
+
+#rows and column sums
+colSums(is.na(restData)) # sum of number of NA
+all(colSums(is.na(restData)) == 0) # is there any NA?
+
+#values with specific characteristics
+table(restData$zipCode %in% c("21212","21213"))
+
